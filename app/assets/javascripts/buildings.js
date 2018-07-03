d1ccc597d622702fa291e074f7233e34a6c0520a
Roberto Carrillo <roberto@kheper.io>
2018-07-03 12:24:02 -0500

    add click event in all proyects

--- app/assets/javascripts/buildings.js
+++ app/assets/javascripts/buildings.js
@@ -1,9 +1,21 @@
 
-$(function() {
-
+$( document ).on('turbolinks:load', function() {
+  //
   $(".kinds-titles").first().addClass('active');
 
   $(".kind-row").hide();
   $(".kind-row").first().hide();
 
-});
+  $(".kinds-titles").click(function() {
+
+      var target = $(this).data('target');
+      console.log(target);
+      $(".kinds-titles").removeClass('active');
+      $('h4.kind-' + target).addClass('active');
+      $('.kind-row').hide();
+      $('div.kind-' + target).show();
+
+  });
+
+})
+
