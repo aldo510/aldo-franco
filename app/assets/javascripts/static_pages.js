236a5249993cec2963be159d323325c70504fac9
Aldo Fermín  Márquez Aguilar <aldo.619_@ciencias.unam.mx>
2018-07-09 15:20:14 -0500

    about us section

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,8 +1,10 @@
 
 $( document ).on('turbolinks:load', function() {
 
-  $('#click-here').click(function() {
-    $("#intro").hide();
-  });
-
-})
+	$('#click-here').click(function() {
+		$("#intro").hide();
+	});
+	$('.cat-1').hover(function(){
+		$('#parent-div').css({'background-image': "image-url(static_pages/home/RESIDENCIAL.jpg);"})
+	});
+});
