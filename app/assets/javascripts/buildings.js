5696b0b2d7bca2f5119555d5b53cf09aa5f1357b
Roberto Carrillo <roberto@kheper.io>
2018-07-12 17:58:09 -0500

    adjust home

--- app/assets/javascripts/buildings.js
+++ app/assets/javascripts/buildings.js
@@ -38,5 +38,23 @@ $( document ).on('turbolinks:load', function() {
     adjustSliderHeight();
   }
 
+  console.log(getUrlParameter('asd'));
+
 })
+
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
 
