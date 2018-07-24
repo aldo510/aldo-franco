8387dafe4cca7d8f69d5b5ac915eedab51b905a1
Roberto Carrillo <roberto@kheper.io>
2018-07-24 18:11:22 -0500

    corrections

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,15 +4,19 @@ $( document ).on('turbolinks:load', function() {
 	$('#click-here').click(function() {
 		$("#intro").hide();
 	});
+	
 	$('.cat-1').hover(function(){
 		$('#parent-div').css({'background-image': "image-url(static_pages/home/RESIDENCIAL.jpg);"})
 	});
+	
 	$('.work-content').hover(function () {
 		$("#work-div").fadeIn();
 	});
+	
 	$("#work-div").mouseleave(function(){
 		$("#work-div").fadeOut();
 	});
+	
 	$('.sections').hover(function () {
 		var id = $(this).data('target');
 		// body...
@@ -23,10 +27,15 @@ $( document ).on('turbolinks:load', function() {
 			$('.'+id).hide();
 		});
 	})
+
 	$('.white-navbar').click(function () {
 		var id = $(this).data('target');
-		$('html, body').animate({scrollTop: $('#' + id).offset().top}, 'slow');
+		var section = $('#' + id);
+		if (section.length > 0){
+			$('html, body').animate({scrollTop: $('#' + id).offset().top}, 'slow');
+		}
 	});
+	
 	$(".lista > li").click(function() {
 		var text = $(this).data('text');
 		$(".about-text").hide();
