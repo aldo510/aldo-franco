6bda7622b90c02a47ba09d648e09c5a569341aca
Aldo <aldo.619_@ciencias.unam.mx>
2018-08-21 18:40:52 -0500

    change slider on home

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,7 +4,28 @@ $( document ).on('turbolinks:load', function() {
 	$('#click-here').click(function() {
 		$("#intro").hide();
 	});
-	
+	$('.banner-slider-2').slick({
+		autoplay: true,
+		infinite: true,
+		dots: false,
+		arrows: false
+	});
+	$('#left-arrow-2').click(function() {
+		$('.banner-slider-2').slick('slickPrev');
+	});
+
+	$('#right-arrow-2').click(function() {
+		$('.banner-slider-2').slick('slickNext');
+	});
+
+	$('#close-arrow').click(function () {
+		$('.banner-slider-2').fadeOut();
+		$('#close-arrow').hide();
+		$('#left-arrow-2').hide();
+		$('#right-arrow-2').hide();
+		$('#banner-clic').css('z-indez', '1');
+		$('#banner-clic').hide();
+	});
 	$('.cat-1').hover(function(){
 		$('#parent-div').css({'background-image': "image-url(static_pages/home/RESIDENCIAL.jpg);"})
 	});
@@ -23,61 +44,57 @@ $( document ).on('turbolinks:load', function() {
 		$("#work-div-2").mouseleave(function(){
 			$("#work-div-2").hide();
 		});
-			// $(".nav-hide").mouseleave(function(){
-			// 	$("#work-div-2").hide();
-			// });
-			$(window).scroll(function(e) {
-				console.log(e);
-				if ($(window).scrollTop() > 30) {
-					$("#work-div-2").hide();
-				}
-			});
+		$(window).scroll(function(e) {
+			if ($(window).scrollTop() > 30) {
+				$("#work-div-2").hide();
+			}
+		});
 
-			$('.sections').hover(function () {
-				var id = $(this).data('target');
-				$('.sections-content').hide();
-				$('.'+id).show();
-				$('.'+id).mouseleave(function () {
-					$('.'+id).hide();
-				});
-			});
-			$('.sections-2').hover(function () {
-				var id = $(this).data('target');
-				$('.sections-content-2').hide();
-				$('.'+id).show();
-				$('.'+id).mouseleave(function () {
-					$('.'+id).hide();
-				});
-			});
-		};
-
-
-
-		$('.white-navbar, .footer-nav-link').click(function () {
+		$('.sections').hover(function () {
 			var id = $(this).data('target');
-			var section = $('#' + id);
-			if (section.length > 0){
-				$('html, body').animate({scrollTop: $('#' + id).offset().top}, 'slow');
-			}
+			$('.sections-content').hide();
+			$('.'+id).show();
+			$('.'+id).mouseleave(function () {
+				$('.'+id).hide();
+			});
 		});
+		$('.sections-2').hover(function () {
+			var id = $(this).data('target');
+			$('.sections-content-2').hide();
+			$('.'+id).show();
+			$('.'+id).mouseleave(function () {
+				$('.'+id).hide();
+			});
+		});
+	};
 
-		$(".lista > li").click(function() {
-			var text = $(this).data('text');
-			$(".about-text").hide();
-			$("#" + text).show();
-			$(".lista > li").removeClass('active-item-li');
-			$(this).addClass('active-item-li');
-		});
 
-		$( window ).resize(function() {
 
-		});
-		$('.navbar-toggler').click(function () {
-			if ($('#transparent-navbar').hasClass('back-black')) {
-				$('#transparent-navbar').removeClass('back-black');
-			}else{
-				$('#transparent-navbar').addClass('back-black');
-			}
-		});
+	$('.white-navbar, .footer-nav-link').click(function () {
+		var id = $(this).data('target');
+		var section = $('#' + id);
+		if (section.length > 0){
+			$('html, body').animate({scrollTop: $('#' + id).offset().top}, 'slow');
+		}
+	});
+
+	$(".lista > li").click(function() {
+		var text = $(this).data('text');
+		$(".about-text").hide();
+		$("#" + text).show();
+		$(".lista > li").removeClass('active-item-li');
+		$(this).addClass('active-item-li');
+	});
+
+	$( window ).resize(function() {
 
 	});
+	$('.navbar-toggler').click(function () {
+		if ($('#transparent-navbar').hasClass('back-black')) {
+			$('#transparent-navbar').removeClass('back-black');
+		}else{
+			$('#transparent-navbar').addClass('back-black');
+		}
+	});
+
+});
