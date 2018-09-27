3f18eee41c116b08beacb4c9c615e991121e071e
Aldo <aldo.619_@ciencias.unam.mx>
2018-09-27 12:40:43 -0500

    change mail to contact

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
 
-  default to: 'roberto_car_av@hotmail.com', subject: 'Contact Form'
+  default to: 'afranco@aldofranco.com', subject: 'Contact Form'
 
   def contact(name, email, comments)
     @name = name
